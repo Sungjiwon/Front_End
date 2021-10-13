09731a394c6d41ca4c3f75b3acb85bf695cd87bc
Sungjiwon <89440718+Sungjiwon@users.noreply.github.com>
2021-10-13 15:27:16 +0900

    Add files via upload

--- homepage3/js/common.js
+++ homepage3/js/common.js
@@ -1,12 +1,35 @@
+// 로드시 한 글자씩 타이핑
+window.addEventListener('load', function() {
+    setTimeout(function() {
+            scrollTo(0, 0)
+    }, 10)
+    var typingBool = false; 
+    var typingIdx=0; 
+    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+       typingBool=true; 
+       
+       var tyInt = setInterval(typing,100); // 반복동작 
+     } 
+     
+     function typing(){ 
+       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+         $(".typing").append(typingTxt[typingIdx]); // 한 글자씩 이어준다. 
+         typingIdx++; 
+       } else{ 
+         clearInterval(tyInt); //끝나면 반복종료 
+       } 
+     }
+})
 let n = 0;
 let state = 1;
-let logo_state = 1;
 let screen_pos = $(".section").position().top
 let footer_pos = $("#footer").height();
 console.log($("body > .section:eq(0)").position().top)
-// 0~7까지.
+
+// 스크롤시 한 페이지씩 움직이기
 document.addEventListener('wheel', function(e) {
-    // console.log(e.wheelDelta)
     if (e.wheelDelta < 0 && state == 1) {
         state = 0;
         n++;
@@ -36,6 +59,7 @@ document.addEventListener('wheel', function(e) {
     }
     $("#sideNav ul li a").removeClass("side_active");
     $("#sideNav ul li a:eq("+n+")").addClass('side_active')
+    
     if(n == 4){
         $("#rightBox ul:eq(0)").addClass('active1')
         $("#rightBox ul:eq(1)").addClass('active2')
@@ -45,34 +69,30 @@ document.addEventListener('wheel', function(e) {
         $("#rightBox ul:eq(1)").removeClass('active2')
     }
     if(n>0) {
-        console.log(253443); 
-        $("#sideNav").animate({opacity:1}, 1000)
-        $("#logo").animate({left:30, top: 16+"%"}, 1000)
+        // $("#sideNav").animate({opacity:1}, 1000)
+        $("#sideNav").show(1000)
+        $("#logo").animate({left:30, top: 15+"%"}, 1000)
     }
     else {
-        console.log(989999999999999999999999999999); 
-        $("#sideNav").css({opacity:0})
+        // $("#sideNav").css({opacity:0})
+        $("#sideNav").hide(1000)
         $("#logo").animate({left:0, top: 0}, 1000)    
     }
     e.preventDefault();
 },{passive : false})
 
-
-
+// header 네비게이션 메뉴 클릭시
 $("#header li").on("click", function(e) { 
     let idName = $(this).find('a').attr('href')
-    // let section_pos = $(idName).position().top
     let section_pos = $(idName).position().top;
     
     $("html, body").animate({ scrollTop: section_pos }, 1000)
     n = section_pos / screen_pos; //1,2,3,4,5,6,7,8
     console.log(section_pos, n)
-    // n = $(this).index();
     $("#sideNav ul li a").removeClass("side_active");
     $("#sideNav ul li a:eq("+n+")").addClass('side_active')
     
     if(n == 4){
-        // $("#rightBox ul").addClass('active')
         $("#rightBox ul:eq(0)").addClass('active1')
         $("#rightBox ul:eq(1)").addClass('active2')
     }
@@ -81,94 +101,71 @@ $("#header li").on("click", function(e) {
         $("#rightBox ul:eq(1)").removeClass('active2')
     }
     if(n>0) {
-        $("#sideNav").animate({opacity:1}, 1000)
-        $("#logo").animate({left:30, top: 16+"%"}, 1000)
+        $("#sideNav").show(1000)
+        $("#logo").animate({left:30, top: 15+"%"}, 1000)
     }
     else {
-        $("#sideNav").css({opacity:0})
+        $("#sideNav").hide(1000)
         $("#logo").animate({left:0, top: 0}, 1000)
     }
     e.preventDefault();
     e.stopPropagation();
 })
 $("#logo").on("click", function() { 
+    n=0;
     $("html, body").animate({ scrollTop: 0 }, 1000)
-    $("#sideNav").css({opacity:0})
+    $("#sideNav").hide(1000)
     $("#logo").animate({left:0, top: 0}, 1000)
+    $("#sideNav ul li a").removeClass("side_active");
+    $("#sideNav ul li a:eq("+n+")").addClass('side_active')
 })
-
-window.addEventListener('load', function() {
-    setTimeout(function() {
-            scrollTo(0, 0)
-    }, 10)
-    var typingBool = false; 
-    var typingIdx=0; 
-    var typingTxt = $(".typing-txt").text(); // 타이핑될 텍스트를 가져온다 
-    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
-       typingBool=true; 
-       
-       var tyInt = setInterval(typing,100); // 반복동작 
-     } 
-     
-     function typing(){ 
-       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-         $(".typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-         typingIdx++; 
-       } else{ 
-         clearInterval(tyInt); //끝나면 반복종료 
-       } 
-     }
-})
-// 네비게이션
+// 헤더에 오버시 서브메뉴까지 나타나기
 $("#header").hover(function() { 
-    $(this).animate({height:225}, 300)
-    $(".snb").show();
-    $("#header .temp_line").show()
+    $("#header:not(:animated)").animate({height:225}, 300)
+    $(".snb:not(:animated)").show();
+    $("#header .temp_line:not(:animated)").show()
     }, function() { 
-        $(this).animate({height: 80}, 300)
+        $(this).css({height: 80})
         $(".snb").hide();
         $("#header .temp_line").hide()
 })
 
 // 사이드 네비게이션
 $("#sideNav ul li").on('click', function(e) {
-    $(this).children('a').addClass('side_active')
-    $(this).siblings().children('a').removeClass("side_active");
+    // 여기서 state는 중복 클릭 방지용.
+    if(state == 1){
+        state = 0;
+        $(this).children('a').addClass('side_active')
+        $(this).siblings().children('a').removeClass("side_active");
 
-    let idName = $(this).children('a').attr('href')
-    let section_pos = $(idName).position().top
-    
-    $("html, body").animate({ scrollTop: section_pos }, 1000)
-    n = $(this).index();
-    console.log(section_pos, n)
-    // console.log($(this).children('a'))
-    // if(n == 4){
-    //     $("#rightBox ul").addClass('active')
-    // }
-    // else $("#rightBox ul").removeClass('active')
-    if(n == 4){
-        // $("#rightBox ul").addClass('active')
-        $("#rightBox ul:eq(0)").addClass('active1')
-        $("#rightBox ul:eq(1)").addClass('active2')
-    }
-    else {
-        $("#rightBox ul:eq(0)").removeClass('active1')
-        $("#rightBox ul:eq(1)").removeClass('active2')
-    }
-    if(n>0) {
-        $("#sideNav").animate({opacity:1}, 1000)
-        $("#logo").animate({left:30, top: 16+"%"}, 1000)
-    }
-    else {
-        $("#sideNav").css({opacity:0})
-        $("#logo").animate({left:0, top: 0}, 1000)
+        let idName = $(this).children('a').attr('href')
+        let section_pos = $(idName).position().top
+        
+        $("html, body").animate({ scrollTop: section_pos }, 1000, function() {
+            state = 1;
+        })
+        n = $(this).index();
+        if(n == 4){
+            $("#rightBox ul:eq(0)").addClass('active1')
+            $("#rightBox ul:eq(1)").addClass('active2')
+        }
+        else {
+            $("#rightBox ul:eq(0)").removeClass('active1')
+            $("#rightBox ul:eq(1)").removeClass('active2')
+        }
+        if(n>0) {
+            $("#sideNav").show(1000)
+            $("#logo").animate({left:30, top: 15+"%"}, 1000)
+        }
+        else {
+            $("#sideNav").hide(1000);
+            $("#logo").animate({left:0, top: 0}, 1000)
+        }
     }
     e.preventDefault();
 })
-// 시도1: 그냥 n>0 152번 if문에 추가해본다.
-// 시도2: if(n == 1 && logo_state == 1) 로 만들어서 따로 해본다.
 
+// 현재상영작 예고편 보기 
 $("#movieList li .explain .trailer").on('click', function(e) {
     let temp_index = $(this).parent().parent().index();
     console.log(temp_index)
@@ -181,29 +178,25 @@ $("#movieList li .explain .trailer").on('click', function(e) {
     e.preventDefault();
 })
 
+// 예고편 X버튼 클릭
 $("#movieList li .teaser .close").on("click", function(e){
     $("#live .darkBg").hide()
     $(this).parent().hide();    
     e.stopPropagation(); // 부모요소인 li 클릭도 인식해서 hide된후 다시 show 된다. 이를 방지 하기 위한 것
 })
 
+// 현재상영영화 슬라이드
 let slideHeight = $("#liveWrap").height();
-console.log(slideHeight);
 let slideIndex = 0;
 let slide_state = 1;
-
 $("#posBtn .prevBtn").on("click", function(e) { 
     prevSlide();
     e.preventDefault();
 })
-
 $("#posBtn .nextBtn").on("click", function(e) { 
     nextSlide();
     e.preventDefault();
 })
-
-// if($("html, body").)
-
 function prevSlide() { 
     if(slide_state == 1){
         slide_state = 0
@@ -218,16 +211,13 @@ function prevSlide() {
         })    
     }
 }
-
 function nextSlide() { 
     if(slide_state == 1){
         slide_state = 0
         console.log(slideIndex)
         slideIndex++;
 
-        if(slideIndex >= 3) slideIndex = 2; // 슬라이드 갯수보다 많을 때 멈추게
-        // $("#live").animate({top: -550},1000)
-        
+        if(slideIndex >= 3) slideIndex = 2; // 슬라이드 갯수보다 많을 때 멈추게        
 
         $("#numBtn li a:eq("+slideIndex+")").text("●");
         $("#numBtn li a").not($("#numBtn li a:eq("+slideIndex+")")).text("○");
@@ -240,7 +230,6 @@ function nextSlide() {
 $("#numBtn li a").on("click", function(e) { 
     console.log($(this).parent().index())
     slideIndex = $(this).parent().index()
-    // if($(this).parent().index() == )
 
     if( slide_state == 1){
         slide_state = 0;
@@ -262,6 +251,7 @@ $('#numBtn a').on('mouseenter', function() {
     $(this).stop().animate({ opacity: 0.7 }, 500);
 });
 
+// 더보기 버튼
 $(".info").each(function(){
     var content = $(this).find('.summary');
     var content_text = content.text();
@@ -293,47 +283,7 @@ $(".info").each(function(){
         }
     }    
 })
-
-var liWidth = $("#galeryList li:first").width();
-var liLength = $("#galeryList li").length;
-console.log( liWidth, liLength)
-
-$("#galery_slider_btn .galery_prev:not(:animated)").on("click", function(e) {
-    h_prevSlide();
-    e.preventDefault();
-})
-
-function h_prevSlide() { 
-    // $("#galeryList li:first:not(:animated)").addClass("galery_active")
-    //                          .siblings().removeClass("galery_active")
-    $("#galeryList:not(:animated)").prepend($("#galeryList li:last"))
-                    .css({marginLeft: -(liWidth+60)})
-                    .animate({marginLeft: 0},1000)
-                    $("#galeryList li:eq(1):not(:animated)").addClass("galery_active")
-                    .siblings().removeClass("galery_active")
-
-    // $("#galeryList").prepend($("#galeryList li:last"))
-    //                 .css({marginLeft: -(liWidth+60)})
-    //                 .animate({marginLeft: 0},1000)
-    // $("#galeryList li:eq(1) img").animate({width:500, height:450}, 1000)
-}
-$("#galery_slider_btn .galery_next:not(:animated)").on("click", function(e) {
-    h_nextSlide();
-    e.preventDefault();
-})
-
-function h_nextSlide() { 
-    $("#galeryList li:eq(2):not(:animated)").addClass("galery_active")
-                             .siblings().removeClass("galery_active")
-    $("#galeryList:not(:animated)").animate({marginLeft: -(liWidth+60)}, 1000, function() {
-            $(this).append($('#galeryList li:first')).css({marginLeft: 0})
-    })
-}
-
-// $("#futureList li").on('click', function(e){
-//     $(this).children('div').html('<iframe width="900" height="500"name="eternals" src="https://www.youtube.com/embed/BdkSkI61aGo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-// })
-
+// 상영예정영화 포스터 클릭시 예고편
 $("#futureList li").on('click', function() { 
     $(this).children('.teaser').show(500);
     $("#futureWrap .darkBg").show()
@@ -347,3 +297,35 @@ $("#futureList li .teaser .close").on('click', function(e){
     $(this).parent().hide();    
     e.stopPropagation(); // 부모요소인 li 클릭도 인식해서 hide된후 다시 show 된다. 이를 방지 하기 위한 것
 })
+
+// 갤러리 슬라이드
+var liWidth = $("#galeryList li:first").width();
+var liLength = $("#galeryList li").length;
+console.log( liWidth, liLength)
+
+$("#galery_slider_btn .galery_prev:not(:animated)").on("click", function(e) {
+    h_prevSlide();
+    e.preventDefault();
+})
+
+function h_prevSlide() { 
+    $("#galeryList:not(:animated)").prepend($("#galeryList li:last"))
+                    .css({marginLeft: -(liWidth+60)})
+                    .animate({marginLeft: 0},1000)
+                    $("#galeryList li:eq(1):not(:animated)").addClass("galery_active")
+                    .siblings().removeClass("galery_active")
+}
+$("#galery_slider_btn .galery_next:not(:animated)").on("click", function(e) {
+    h_nextSlide();
+    e.preventDefault();
+})
+function h_nextSlide() { 
+    $("#galeryList li:eq(2):not(:animated)").addClass("galery_active")
+                            .siblings().removeClass("galery_active")
+    $("#galeryList:not(:animated)").animate({marginLeft: -(liWidth+60)}, 1000, function() {
+            $(this).append($('#galeryList li:first')).css({marginLeft: 0})
+    })
+}
+
+
+
